f87ed95f6dab4450ca7eb6a290217843037950f9
James Hodgkinson <james@terminaloutcomes.com>
2021-04-16 23:00:32 +1000

    fixing a little bug

--- build_in_docker.sh
+++ build_in_docker.sh
@@ -2,4 +2,11 @@
 
 # builds the packages in docker
 mkdir -p ./output
+
+if [ ! -f ".env" ]; then
+    echo "Couldn't find a .env file, making a blank one"
+    touch .env
+fi
+
+docker-compose build
 docker-compose up

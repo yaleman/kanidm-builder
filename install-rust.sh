522b1d8b58a40d44878e97b477b9f8d7d188740a
James Hodgkinson <james@terminaloutcomes.com>
2021-04-18 14:25:14 +1000

    more tweaks

--- install-rust.sh
+++ install-rust.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+VERSION="1.49.0"
+
+echo "Installing rust ${VERSION} with rustup"
 curl --proto '=https' --tlsv1.2 --output /tmp/rustup.sh -sSf https://sh.rustup.rs
 chmod +x /tmp/rustup.sh
 /tmp/rustup.sh -y
 rm /tmp/rustup.sh
 
-
 PATH=/root/.cargo/bin:$PATH
 export PATH
 
-
 rustup update
-rustup default 1.49.0
+rustup default "${VERSION}"

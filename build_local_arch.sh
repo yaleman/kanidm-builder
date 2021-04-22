606f8a634090b95ad09a50f84862fb7577d32cf5
James Hodgkinson <james@terminaloutcomes.com>
2021-04-22 14:52:30 +1000

    fixing missing line break

--- build_local_arch.sh
+++ build_local_arch.sh
@@ -155,9 +155,10 @@ echo "Copying build artifacts to s3"
 aws --endpoint-url "${S3_HOSTNAME}" \
     --no-verify-ssl \
     s3 sync \
-     --exclude "**/target/release/**"
+     --exclude "**/target/release/**" \
     "${BUILD_DIR}/target/" \
     "s3://kanidm-builds/${OSID}/${VERSION}"
+
 echo "Copying build logs to s3"
 aws --endpoint-url "${S3_HOSTNAME}" \
     --no-verify-ssl \

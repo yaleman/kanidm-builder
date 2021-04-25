90ce01d3a7e1d4e21cb95ca003fb6cf024ebe8bf
James Hodgkinson <james@terminaloutcomes.com>
2021-04-26 07:56:51 +1000

    recovery mode

--- recovery_mode.sh
+++ recovery_mode.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-docker run --rm -it --env-file=.env --volume "$1:/data" $1  bash
-
 echo "######################################"
-echo "RECOVERY COMMANDS"
+echo " RECOVERY COMMANDS"
 echo "######################################"
 
 #shellcheck disable=SC2016
@@ -14,3 +12,9 @@ echo 'aws --endpoint-url "${S3_HOSTNAME}" \
         s3 sync \
         "/data/opensuse-tumbleweed/unknown/target/release" \
         "s3://kanidm-builds/${OSID}/${VERSION}/$(uname -m)/" 2>&1'
+
+echo "######################################"
+echo " RUNNING CONTAINER"
+echo "######################################"
+docker run --rm -it --env-file=.env --volume "$1:/data" $1  bash
+

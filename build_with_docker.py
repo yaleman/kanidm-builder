633769885c729504807213c1040c9026280ee101
James Hodgkinson <james@terminaloutcomes.com>
2021-08-13 07:59:32 +1000

    adding some more error logging

--- build_with_docker.py
+++ build_with_docker.py
@@ -198,7 +198,7 @@ def build_version(version_string: str, force_container_build: bool):
             return False
         except Exception as build_error:
             logger.error("Exception for {}: {}", version_tag, build_error)
-            if 'returned a non-zero code: 104' in build_error:
+            if 'returned a non-zero code: 104' in f"{build_error}":
                 logger.error("Zypper returned 104, which means package not found.")
             return False
         logger.debug("Image: {}", image)

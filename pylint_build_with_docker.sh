d76a36ca6063ed3f53b8f04bd8ccbc3c1a068dd5
James Hodgkinson <james@terminaloutcomes.com>
2022-04-02 12:32:14 +1000

    adding shellcheck github_action and cleaning up shellcheck errors

--- pylint_build_with_docker.sh
+++ pylint_build_with_docker.sh
@@ -3,8 +3,12 @@
 if [ ! -d venv ]; then
 	echo "Couldn't find virtualenv, creating"
 	virtualenv venv
+
+	# shellcheck disable=SC1091
 	source venv/bin/activate
 	python3 -m pip install -r requirements.txt pylint black
 fi
+
+# shellcheck disable=SC1091
 source venv/bin/activate
 python3 -m pylint build_with_docker.py --disable=W0511
